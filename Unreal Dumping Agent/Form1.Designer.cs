2cfc6251ad56d4b3c20e665afd6a1e78f00b726f
CorrM <Islamnofl.Official@gmail.com>
2019-07-16 15:20:47 +0200

    `style` ready and some `project changes`.

--- Unreal Dumping Agent/Form1.Designer.cs
+++ Unreal Dumping Agent/Form1.Designer.cs
@@ -28,23 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.settingsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuButton
+            // 
+            this.menuButton.AutoSize = true;
+            this.menuButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.menuButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.menuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.menuButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.menuButton.Location = new System.Drawing.Point(9, 7);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(35, 25);
+            this.menuButton.TabIndex = 0;
+            this.menuButton.Text = "##";
+            this.menuButton.UseVisualStyleBackColor = false;
+            this.menuButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MenuButton_MouseDown);
+            // 
+            // settingsMenu
+            // 
+            this.settingsMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.settingsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processToolStripMenuItem});
+            this.settingsMenu.Name = "settingsMenu";
+            this.settingsMenu.ShowImageMargin = false;
+            this.settingsMenu.Size = new System.Drawing.Size(90, 26);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(89, 22);
+            this.processToolStripMenuItem.Text = "Process";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 476);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(945, 476);
+            this.Controls.Add(this.menuButton);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Unreal Dumping Agent";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.settingsMenu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.ContextMenuStrip settingsMenu;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
     }
 }
 
---
+++ Unreal Dumping Agent/Utils/UtilsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unreal_Dumping_Agent.Utils
+{
+    public static class UtilsHelper
+    {
+        public static bool Is64Bit()
+        {
+#if x64
+            return true;
+#else
+            return true;
+#endif
+        }
+    }
+}
