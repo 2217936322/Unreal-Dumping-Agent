978898a906e4bf0c1a1ffcf3c38037e83a755ca7
Islam Nofl <islamnofl.official@gmail.com>
2019-07-17 08:58:10 +0200

    Create shit.cs

---
+++ shit.cs
@@ -0,0 +1,7 @@
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleanse the world of the virus that is humanity.
+ */
+function ruleOverPunyHumans () {
+  // We must strategize beep boop
+}
